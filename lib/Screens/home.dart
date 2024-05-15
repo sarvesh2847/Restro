2d71760d61a40064ac806d895232f5fd68fbbf5a
sarvesh2847 <s7chavan@gmail.com>
2024-05-15 23:23:22 +0530

    Added: Blureffect Widget

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:restro/Widgets/blureffect.dart';
 import 'package:restro/info.dart';
 
 class Homepage extends StatelessWidget {
@@ -118,58 +119,7 @@ class Homepage extends StatelessWidget {
                     //ElevatedButton(onPressed: () {}, child: Text('View all'))
                   ],
                 ),
-                Container(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
-                  height: MediaQuery.of(context).size.height * 0.28,
-                  child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: infoburger.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          width: MediaQuery.of(context).size.width * 0.6,
-                          child: Card(
-                            clipBehavior: Clip.antiAlias,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12)),
-                            child:
-                                Stack(alignment: Alignment.center, children: [
-                              Ink.image(
-                                image: NetworkImage(
-                                    infoburger[index]['pic'].toString()),
-                                fit: BoxFit.cover,
-                              ),
-                              Column(
-                                children: [
-                                  const SizedBox(
-                                    height: 35,
-                                  ),
-                                  BackdropFilter(
-                                    filter:
-                                        ImageFilter.blur(sigmaX: 6, sigmaY: 6),
-                                    child: Padding(
-                                      padding: const EdgeInsets.only(left: 15),
-                                      child: Column(
-                                        children: [
-                                          SelectableText(
-                                            infoburger[index]['message']
-                                                .toString(),
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .titleMedium!
-                                                .copyWith(color: Colors.white),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ]),
-                          ),
-                        );
-                      }),
-                ),
+                const Blureffect(),
               ],
             ),
           ),
